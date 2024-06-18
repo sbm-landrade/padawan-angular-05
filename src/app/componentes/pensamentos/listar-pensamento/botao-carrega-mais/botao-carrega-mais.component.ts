592237bcc872fc4f3e9f7a440369b963a443adbb
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-18 15:32:35 -0300

    Botão para carregar mais dados "criação do componente"

---
+++ src/app/componentes/pensamentos/listar-pensamento/botao-carrega-mais/botao-carrega-mais.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-carrega-mais',
+  templateUrl: './botao-carrega-mais.component.html',
+  styleUrls: ['./botao-carrega-mais.component.css']
+})
+export class BotaoCarregaMaisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
