2a4264ea27dc58e366995c4fbe8ada6ef8f5c802
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-19 00:42:13 -0300

    Adicionando ícone de favorito

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,9 +1,7 @@
 import { Router } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
-import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -17,7 +15,7 @@ export class CriarPensamentoComponent implements OnInit {
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -36,16 +34,17 @@ export class CriarPensamentoComponent implements OnInit {
   }
 
   criarPensamento() {
-    console.log(this.formulario.get('autoria')?.errors);
-    if(this.formulario.valid)
-    this.service.criar(this.formulario.value).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
+    if(this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelar() {
     this.router.navigate(['/listarPensamento'])
   }
+
   habilitarBotao(): string {
     if(this.formulario.valid) {
       return 'botao'
@@ -53,4 +52,5 @@ export class CriarPensamentoComponent implements OnInit {
       return 'botao__desabilitado'
     }
   }
+
 }
